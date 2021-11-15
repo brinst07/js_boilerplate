a51d20fc97fe88b67e330cfd02fc008e33308988
brinst <brinst07@gmail.com>
2021-11-15 23:37:33 +0900

    feat: multer 추가 및 github action Test

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -2,8 +2,12 @@ import { Module } from '@nestjs/common';
 import { UserModule } from './user/user.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { typeORMConfig } from './config/typeORMConfig';
+import { FileController } from './file/file.controller';
+import { FileService } from './file/file.service';
 
 @Module({
   imports: [TypeOrmModule.forRoot(typeORMConfig), UserModule],
+  controllers: [FileController],
+  providers: [FileService],
 })
 export class AppModule {}
---
+++ backend/src/file/file.controller.ts
@@ -0,0 +1,19 @@
+import {
+  Controller,
+  Post,
+  UploadedFile,
+  UseInterceptors,
+} from '@nestjs/common';
+import { FileService } from './file.service';
+import { FileInterceptor } from '@nestjs/platform-express';
+
+@Controller('file')
+export class FileController {
+  constructor(private fileService: FileService) {}
+
+  @Post('upload')
+  @UseInterceptors(FileInterceptor('file'))
+  uploadFile(@UploadedFile() file: Express.Multer.File) {
+    console.log(file);
+  }
+}
